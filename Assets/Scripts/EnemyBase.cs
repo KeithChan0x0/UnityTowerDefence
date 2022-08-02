486ea6cfa93464dcb10dc454a229decee7be3083
YukiNekoMochi <nekomochi.cat222@gmail.com>
2022-08-02 15:09:02 +0900

    敵の動きの軽い調整

--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -19,18 +19,37 @@ public class EnemyBase : MonoBehaviour
 	// ターゲットのセット
 	protected void TargetSet()
 	{
-		targetPos = GameObject.FindWithTag("Wall").transform.position;
+		targetPos.x = GameObject.FindWithTag("Wall").transform.position.x;
 	}
 
 	// 移動処理
 	protected void CharacterMove()
 	{
-		float length = Vector3.Distance(targetPos, transform.position);
+		float length = 0.0f;
+#if false
+		length = Vector3.Distance(targetPos, transform.position);
+#else
+		Vector3 target = targetPos;
+		target.y = 0.0f;
+		target.z = 0.0f;
+		Vector3 pos = transform.position;
+		pos.y = 0.0f;
+		pos.z = 0.0f;
+		length = Vector3.Distance(target, pos);
+#endif
 		// 一定距離より遠かったら移動してくる
 		if (length < targetNearLength) return;
+#if false
 		Vector3 dir = targetPos - transform.position;
 		dir = dir.normalized;
 		transform.position += (dir * moveSpeed * Time.deltaTime);
+#else
+		Vector3 dir = targetPos - transform.position;
+		dir.y = 0.0f;
+		dir.z = 0.0f;
+		dir.x = dir.normalized.x;
+		transform.position += (dir * moveSpeed * Time.deltaTime);
+#endif
 	}
 
 }
--- Assets/Scripts/EnemySky.cs
+++ Assets/Scripts/EnemySky.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemySky : EnemyBase
 {
 	// ターゲットとの距離の基準
-	const float TARGET_LENGTH_STANDARD = 20.0f;
+	const float TARGET_LENGTH_STANDARD = 10.0f;
 	// ターゲットとの距離で前後する範囲
 	const float TARGET_LENGTH_AROUND = 5.0f;
 
@@ -14,9 +14,9 @@ public class EnemySky : EnemyBase
     {
 		TargetSet();
 		// ある程度基準距離からずらす
-        targetNearLength = Random.Range(TARGET_LENGTH_STANDARD - TARGET_LENGTH_AROUND,
-										TARGET_LENGTH_STANDARD + TARGET_LENGTH_AROUND);
-    }
+		targetNearLength = TARGET_LENGTH_STANDARD;
+		moveSpeed = 7.0f;
+	}
 
 	void Attack()
 	{
@@ -27,7 +27,24 @@ public class EnemySky : EnemyBase
     void Update()
     {
         CharacterMove();
+		float length = 0.0f;
+		Vector3 target = targetPos;
+		target.y = 0.0f;
+		target.z = 0.0f;
+		Vector3 pos = transform.position;
+		pos.y = 0.0f;
+		pos.z = 0.0f;
+		length = Vector3.Distance(target, pos);
+		//　空中は一定量近づいたら壁に迫ってくる
+		if (length < targetNearLength && length > 1.0f)
+		{
+			target.y = 3.0f;
+			Vector3 dir = target - transform.position;
+			dir = dir.normalized;
+			dir.z = 0.0f;
+			transform.position += (dir * moveSpeed * Time.deltaTime);
+		}
 		// 攻撃処理
-
+		Attack();
     }
 }
