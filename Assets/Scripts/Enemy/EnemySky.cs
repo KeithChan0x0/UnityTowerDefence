14b05e416b672f9a74ff96885b6a1165e3d632e8
YukiNekoMochi <nekomochi.cat222@gmail.com>
2022-08-03 13:53:10 +0900

    敵関連の追加

--- Assets/Scripts/Enemy/EnemySky.cs
+++ Assets/Scripts/Enemy/EnemySky.cs
@@ -4,27 +4,13 @@ using UnityEngine;
 
 public class EnemySky : EnemyBase
 {
-	// ターゲットとの距離の基準
-	const float TARGET_LENGTH_STANDARD = 10.0f;
-	// ターゲットとの距離で前後する範囲
-	const float TARGET_LENGTH_AROUND = 5.0f;
-
 	// Start is called before the first frame update
 	void Start()
     {
-		TargetSet();
-		// ある程度基準距離からずらす
-		targetNearLength = TARGET_LENGTH_STANDARD;
-		moveSpeed = 7.0f;
 	}
 
-	void Attack()
-	{
-
-	}
-
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	public virtual void Update()
     {
 		Move();
 		float length = 0.0f;
@@ -44,7 +30,5 @@ public class EnemySky : EnemyBase
 			dir.z = 0.0f;
 			transform.position += (dir * moveSpeed * Time.deltaTime);
 		}
-		// 攻撃処理
-		Attack();
     }
 }
--- Assets/Scripts/Enemy/EnemyStoneMonster.cs
+++ Assets/Scripts/Enemy/EnemyStoneMonster.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStoneMonster : MonoBehaviour
+public class EnemyStoneMonster : EnemySky
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// ターゲットとの距離の基準
+	const float TARGET_LENGTH_STANDARD = 1.0f;
+	// ターゲットとの距離で前後する範囲
+	const float TARGET_LENGTH_AROUND = 0.0f;
 
-    // Update is called once per frame
-    void Update()
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		TargetSet( TARGET_LENGTH_STANDARD, TARGET_LENGTH_AROUND);
+	}
+
+	// Update is called once per frame
+	public override void Update()
+    {
+		Move();
+	}
 }
