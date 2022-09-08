0d6ddd5fdac30a69d135828cbd6ecba82f73aaed
chin <2004110016cs@edu.oca.ac.jp>
2022-09-08 18:56:13 +0900

    User Interface

--- Assets/Scripts/Enemy/EnemyAction.cs
+++ Assets/Scripts/Enemy/EnemyAction.cs
@@ -6,26 +6,46 @@ using UnityEngine.AI;
 public class EnemyAction : MonoBehaviour
 {
 	public NavMeshAgent navMeshAgent;
-	public GameObject target;
+	public GameObject target1;
+	public GameObject target2;
+
 	public LayerMask whatIsGround, whatIsPlayer;
 
 	public float health;
 
+	public bool moveToTarget2 = false;
 	// Start is called before the first frame update
 	void Start()
-    {
-		target = GameObject.FindGameObjectWithTag("Player");
+	{
+		target2 = GameObject.FindGameObjectWithTag("Player");
 		navMeshAgent = GetComponent<NavMeshAgent>();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
+		//if (!moveToTarget2)
+		//{
+		//	float distanceToTarget = Vector3.Distance(transform.position, target1.transform.position);
+		//	if (distanceToTarget < 0.1f)
+		//	{
+		//		moveToTarget2 = true;
+		//	}
+		//}
+
 		HandleChase();
 	}
 
 	private void HandleChase()
 	{
-		navMeshAgent.SetDestination(target.transform.position);
+		//if (!moveToTarget2)
+		//{
+		//	navMeshAgent.SetDestination(target1.transform.position);
+
+		//}
+		//else
+		{
+			navMeshAgent.SetDestination(target2.transform.position);
+		}
 	}
 }
